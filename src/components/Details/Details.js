5a8a1b150fe910e853cb997ad62bffaf29ae2850
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-05-01 22:03:27 +0300

    basic layouts implemented

---
+++ src/components/Details/Details.js
@@ -0,0 +1,5 @@
+import "./Details.css";
+
+export default function Details() {
+  return <article className="article-details">Details</article>;
+}
---
+++ src/components/List/List.js
@@ -0,0 +1,11 @@
+import "./List.css";
+
+export default function List() {
+  return (
+    <ul className="list">
+      <li className="list-item">list item</li>
+      <li className="list-item">list item</li>
+      <li className="list-item">list item</li>
+    </ul>
+  );
+}
