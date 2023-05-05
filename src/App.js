46478ba8d418d2eb9e3009110657bae6abfdd18f
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2023-05-06 00:34:54 +0300

    info note style update

--- src/App.js
+++ src/App.js
@@ -21,12 +21,13 @@ function App() {
   }, []);
 
   const JSX = (
-    <div className="App">
-      <p>
+    <div>
+      <p className="info">
         Это приложение является частью учебного проекта на{" "}
         <strong>React.js.</strong> Любое совпадение указанных данных с данными
-        реальных людей является непреднамеренным. Исходный код приложения и
-        описание учебного задания доступны{" "}
+        реальных людей является непреднамеренным.
+        <br />
+        Исходный код приложения и описание учебного задания доступны{" "}
         <a
           href="https://github.com/laps78/use-effect-task"
           target="_blank"
@@ -36,12 +37,14 @@ function App() {
         </a>{" "}
         проекта.
       </p>
-      {(isLoading && (
-        <p className="details__preloader">Загружаем данные...</p>
-      )) || <List data={data} selectItem={selectItem} />}
-      {(selectedItem && <Details info={selectedItem} />) || (
-        <p className="details__preloader">Not selected</p>
-      )}
+      <div className="App">
+        {(isLoading && (
+          <p className="app__preloader">Загружаем данные...</p>
+        )) || <List data={data} selectItem={selectItem} />}
+        {(selectedItem && <Details info={selectedItem} />) || (
+          <p className="details__preloader">Not selected</p>
+        )}
+      </div>
     </div>
   );
   return (isLoading && <p className="preloader">Загружаем данные...</p>) || JSX;
